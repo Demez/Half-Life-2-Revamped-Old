2a220034224149960f5683bc9c7e19659901806b
Demez <demizexp@gmail.com>
2017-12-06 15:51:57 -0500

    Replaced src with older version that works and is renamed to src-2013

--- game/hl2-revamped/test_coast.bat
+++ game/hl2-revamped/test_coast.bat
@@ -1,1 +1,1 @@
-@"D:\SteamLibrary\steamapps\common\Source SDK Base 2013 Singleplayer/hl2.exe" -noborder -windowed -dev -allowdebug -game D:\sourceengine\personal-projects\Half-Life-2-Revamped\game\hl2-revamped +god +map d2_coast_01 +sv_infinite_aux_power 1
+@"D:\SteamLibrary\steamapps\common\Source SDK Base 2013 Singleplayer/hl2.exe" -noborder -windowed -dev -allowdebug -game D:\sourceengine\community-projects\Half-Life-2-Revamped\game\hl2-revamped +god +map d2_coast_01 +sv_infinite_aux_power 1
--- src/createtools.bat
+++
@@ -1,1 +0,0 @@
-devtools\bin\vpc.exe +tools /mksln tools.sln
